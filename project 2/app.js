d708573e000698d96d1ec6673dc65027db566666
Sarah Seidman <ss5311@barnard.edu>
2018-04-12 20:45:26 -0400

    axios

--- project 2/app.js
+++ project 2/app.js
@@ -1,1 +1,23 @@
- 
+var movies = new Vue({
+ 	el: "#display",
+ 	data: {
+ 		searched-movie: []
+ 		movies: [
+ 		{title: "Titanic"}],
+ 		search: ""
+
+ 	},
+
+ 	methods: {
+ 		search: function(){
+ 			axios.get(url).then(function(response){
+            this.results = response.data //put this data into movies object
+ 		}
+ 	}
+
+
+ })
+
+ var url = "";
+
+
